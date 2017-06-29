ed76e4c8ef5a0cda19505a1b211929be9f8fc365
Adam Sinck <absinck+github@grueslayer.com>
2017-06-29 15:32:49 -0700

    clean up the code a little

--- cel_webcam.py
+++ cel_webcam.py
@@ -33,17 +33,25 @@ if len(failedPackages) > 0:
     exit()
 
 
+#This is the class for the webcam. Right now, it doesn't do much, but
+#in the future it might.
 class webcam:
     def __init__(self):
+        #initialize the webcam
         self.capture = cv.VideoCapture(0)
+        #take a first frame
         self.frame = self.capture.read()[1]
         # self.height, self.width = self.t.shape[:2]
 
     def takeFrame(self):
+        #take a frame
         self.frame = self.capture.read()[1]
         
 
 class GUI:
+    #This is the main GUI. It takes a frame as an argument, which it
+    #will embed everything into. Not real useful here because of USB
+    #webcam limitations, but so it is. 
     def __init__(self, frame):
         """
         +--------------------------+
@@ -57,58 +65,104 @@ class GUI:
         +--------------------------+
         """
 
+        #Two frames: The frame for the stream, and the frame for the
+        #control panels. 
         self.displayFrame = Frame(frame)
         self.controlFrame = Frame(frame)
-
         self.displayFrame.pack(side=TOP)
         self.controlFrame.pack(side=BOTTOM, fill=X, expand=YES)
 
+
+        #The display is a label.
         self.display = Label(self.displayFrame)
         self.display.after(10, self.stream)
-        self.webcam = webcam()
-        self.mask = []
         self.display.pack()
-
-        self.normal = True
-        self.edges = False
-        self.blurLevel = 5
         
-        self.celButton = Button(self.controlFrame, text="Toggle Cel Shading", command = lambda : self.toggleNormality(None))
+        #This is for working with the frames taken from the webcam. It
+        #gets displayed on self.display.
+        self.pictureframe = None
 
-        self.edgesButton = Button(self.controlFrame, text="Toggle Edges", command = lambda : self.toggleEdges(None))
 
+        #init the webcam in its class
+        self.webcam = webcam()
+
+        #This is what does the Cel masking. The numbers are powers of
+        #two minus 1, so that when the mask is bitwise or'd with a
+        #frame, it will convert the last bits of the H, S, and V to 1's.
+        self.mask = np.array([7, 31, 31])
+
+        #some conditions and variables.
+        #if we're doing cel shading right now
+        self.cel = False
+        #if we put edges on stuff
+        self.edges = False
+        #how much to smooth stuff
+        self.blurLevel = 5
+
+        #add a couple buttons to toggle these
+        self.celButton = Button(self.controlFrame, text="Toggle Cel Shading",
+                                command = lambda : self.toggleCel(None))
+
+        self.edgesButton = Button(self.controlFrame, text="Toggle Edges",
+                                  command = lambda :self.toggleEdges(None),
+                                  state="disabled")
+        
         self.celButton.pack(side=LEFT, fill=BOTH, expand=YES)
         self.edgesButton.pack(side=LEFT, fill=BOTH, expand=YES)
 
-        self.pictureframe = None
+        
+        
+    #These are functions to toggle what the webcam's doing.
+    
+    #Enable/disable cel shading. Also, a user should only be able to
+    #toggle edges while cel shading is active. 
+    def toggleCel(self, event):
+        self.cel = not self.cel
+        if (not self.cel):
+            self.edgesButton.config(state="disabled")
+        else:
+            self.edgesButton.config(state="normal")
 
-    def toggleNormality(self, event):
-        self.normal = not self.normal
-
+    #Enable/disable edges
     def toggleEdges(self, event):
         self.edges = not self.edges
 
+
+    #This is the "main" function. It does the streaming and processing.
     def stream(self):
         #take a frame
         self.webcam.takeFrame()
         frame = self.webcam.frame
-        if (not self.normal):
-            
-            
+
+        #if we're doing cel shading
+        if (self.cel):
+            #get a copy of the frame in HSV, and mask it. The idea
+            #here is to round the HSV values to certain increments, so
+            #that there's a limited set of colors, saturations, and
+            #values (brightnesses). A future version of this will
+            #probably do things a little differently. Right now color
+            #is still a little too dependent on saturation and value. 
             cel = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-            frame = cv.bitwise_or(cel, np.array([7, 31, 31]))
-            #cv.GaussianBlur(, (3, 3), 0)
-            
+            frame = cv.bitwise_or(cel, self.mask)
             frame = cv.cvtColor(frame, cv.COLOR_HSV2BGR)
+
+            #cv.GaussianBlur(, (3, 3), 0)
+
+            #Denoise. A gaussian blur isn't suitable for this, because
+            #I'm going for blotches of color instead of just putting
+            #the frame a little out of focus. 
             frame = cv.medianBlur(frame, self.blurLevel)
-                
+
+            #if we're doing edge detection, do it
             if (self.edges):
+                #Find the edges. 100 and 100 are magic values that
+                #worked nicely for me. 
                 edges = cv.Canny(frame, 100, 100)
                 edges = cv.bitwise_not(edges)
+                
+                #put the edges on the frame
                 frame = cv.bitwise_and(frame, frame, mask=edges)
 
-            # frame = cv.bitwise_and(frame, np.array(edges))
-            
         
         #convert the frame to something tk can use
         self.pictureframe = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)
@@ -124,6 +178,7 @@ class GUI:
         self.display.after(5, self.stream)
 
 
+#start everything
 def main():
     root = Tk()
     root.title("Cel Shading Webcam")
@@ -131,6 +186,7 @@ def main():
     mainFrame.pack()
     window = GUI(mainFrame)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
